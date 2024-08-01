96b5ee4bd1ffb13bf3ab1919c640284c4dedf6f5
InshutiSouvede <isouvede2@gmail.com>
2024-08-01 23:12:07 +0200

    Highlight active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import {NavLink, Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
     <div className="flex justify-between h-20 items-center px-10 text-xl bg-[#FFF7ED]">
         <Link className="uppercase font-bold" to="/">#Vanlife</Link>
         <div className="flex gap-5">
-            <Link to="about">About</Link>
-            <Link to="vans">Vans</Link>
+            <NavLink className={({isActive})=>isActive?'underline':''} to="about">About</NavLink>
+            <NavLink className={({isActive})=>isActive?'underline':''} to="vans">Vans</NavLink>
         </div>
     </div>
     )
