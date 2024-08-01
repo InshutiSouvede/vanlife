5cde54dbb79aba06132ce9b4b7442912ec640a28
InshutiSouvede <isouvede2@gmail.com>
2024-08-01 15:15:55 +0200

    Add vans hero section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-    <div className="bg-[#AAAAAA] py-4 h-14 flex justify-center items-center gap-2">
+    <div className="bg-[#413f3f] py-4 h-14 flex justify-center items-center gap-2 text-white">
         <span className="text-3xl">&copy;</span>
         <h1 className="text-center">
             #VANLIFE
--- src/components/pages/vans/Vans.jsx
+++ src/components/pages/vans/Vans.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Vans(){
     return (
-        <div>
-            <h1 className="font-bold">
-               Vans page
+        <div className="p-10 flex flex-col gap-5">
+            <h1 className="font-bold text-3xl">
+               Explore our vans options
             </h1>
+            <div className="grid grid-cols-4 gap-5 items-center text-[#4D4D4D]">
+                <button className="bg-[#FFEAD0] rounded-md py-2">Simple</button>
+                <button className="bg-[#FFEAD0] rounded-md py-2">Luxury</button>
+                <button className="bg-[#FFEAD0] rounded-md py-2">Rugged</button>
+                <Link className="underline" to=''>Clear filters</Link>
+            </div>
         </div>
     )
 }
