90e6513138ca80f3997324a4839a94ee3e20a02f
InshutiSouvede <isouvede2@gmail.com>
2024-08-01 13:58:32 +0200

    Project initialization

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div>
+        <h1 className="font-bold">
+            About page
+        </h1>
+    </div>
+    )
+  }
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return (
+        <div>
+            <h1 className="font-bold">
+                Home page
+            </h1>
+        </div>
+    )
+}
