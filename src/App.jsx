3738037a8b90cb029d88a2f5e91f093eede2712a
InshutiSouvede <isouvede2@gmail.com>
2024-08-03 12:10:01 +0200

    Page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./components/layouts/AppLayout";
 import Vans from "./components/pages/vans/Vans";
 import VanDetail from "./components/pages/vans/VanDetail";
+import NotFound from "./components/pages/NotFound";
 export default function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AppLayout/>} >
-      <Route path="" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="vans" element={<Vans />} />
-      <Route path="vans/:id" element={<VanDetail />} />
-      </Route>
-      
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route path="" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="vans" element={<Vans />} />
+            <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return (
+        <>
+        <h1 className="font-semibold text-center text-3xl">Page Not found</h1>
+        </>
+    )
+}
