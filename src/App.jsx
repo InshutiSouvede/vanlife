f7fce8685e7524af834f6af27aa94617e9205bc1
InshutiSouvede <isouvede2@gmail.com>
2024-08-03 12:17:15 +0200

    Create browser router and router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/layouts/AppLayout";
 import Vans, { loader } from "./components/pages/vans/Vans";
 import VanDetail from "./components/pages/vans/VanDetail";
 import NotFound from "./components/pages/NotFound";
 export default function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
             <Route path="" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="vans" element={<Vans />} loader={loader} />
             <Route path="vans/:id" element={<VanDetail />} />
           <Route path="*" element={<NotFound />} />
           </Route>
-        </Routes>
-      </BrowserRouter>
+  ))
+  return (
+    <>
+      <RouterProvider router={router} />
     </>
   );
 }
