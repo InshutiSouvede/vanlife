e86e927066a94c461d2204afa2759793d7ac6755
InshutiSouvede <isouvede2@gmail.com>
2024-08-05 09:20:14 +0200

    Dashboard page

--- src/components/layouts/HostLayout.jsx
+++ src/components/layouts/HostLayout.jsx
@@ -3,13 +3,16 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function HostLayout(){
     return(
         <>
-        <div className="flex gap-4">
+        <div className="px-10 flex my-10 gap-4 text-xl">
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} end to="." >Dashboard</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="income" >Income</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="vans" >Vans</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="reviews" >Reviews</NavLink>
         </div>
+        {/* <div className="px-10"> */}
         <Outlet />
+        {/* </div> */}
+        
         </>
     )
 }
--- src/components/pages/hosts/Dashboard.jsx
+++ src/components/pages/hosts/Dashboard.jsx
@@ -1,7 +1,23 @@
-export default function Dashboard(){
-    return(
-        <>
-        <h1 className="font-semibold">Host Dashboard</h1>
-        </>
-    )
+export default function Dashboard() {
+  return (
+    <>
+      <div className="px-10 bg-[#FFEAD0]  flex flex-col gap-5 py-5">
+        <h1 className="font-semibold text-3xl">Welcome!</h1>
+        <div className="flex text-xl justify-between">
+          <p>
+            Income last <span className="underline">30 days</span>
+          </p>
+          <p>Details</p>
+        </div>
+        <p className="font-bold text-3xl">$2,260</p>
+      </div>
+      <div className="p-10 text-xl bg-[#FFDDB2] flex justify-between">
+        <div className="flex">
+            <p>Review score</p>
+            <p><span>5.0</span>/5</p>
+        </div>
+        <p>Details</p>
+      </div>
+    </>
+  );
 }
