31405c2c3c333c79bad79fdbb40a86cfa6842fab
InshutiSouvede <isouvede2@gmail.com>
2024-08-05 08:54:14 +0200

    Add host link on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ export default function Navbar() {
     <div className="flex justify-between h-20 items-center px-10 text-xl bg-[#FFF7ED]">
         <Link className="uppercase font-bold" to="/">#Vanlife</Link>
         <div className="flex gap-5">
+            <NavLink className={({isActive})=>isActive?'underline':''} to="host">Host</NavLink>
             <NavLink className={({isActive})=>isActive?'underline':''} to="about">About</NavLink>
             <NavLink className={({isActive})=>isActive?'underline':''} to="vans">Vans</NavLink>
         </div>
