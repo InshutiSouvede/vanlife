f314cc9527e43f18b711cbf37121859c46f6d1b4
InshutiSouvede <isouvede2@gmail.com>
2024-08-05 09:04:58 +0200

    Add end to prevent index link from constant highlight

--- src/components/layouts/HostLayout.jsx
+++ src/components/layouts/HostLayout.jsx
@@ -4,7 +4,7 @@ export default function HostLayout(){
     return(
         <>
         <div className="flex gap-4">
-            <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="." >Dashboard</NavLink>
+            <NavLink className={({isActive})=>isActive?'underline font-semibold':''} end to="." >Dashboard</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="income" >Income</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="vans" >Vans</NavLink>
             <NavLink className={({isActive})=>isActive?'underline font-semibold':''} to="reviews" >Reviews</NavLink>
