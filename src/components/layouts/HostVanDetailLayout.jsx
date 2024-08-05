00f28008ede5fa6f966ff42b7d6844616e10639c
InshutiSouvede <isouvede2@gmail.com>
2024-08-05 11:26:32 +0200

    Add a back to all vans link on hast van detail

--- src/components/layouts/HostVanDetailLayout.jsx
+++ src/components/layouts/HostVanDetailLayout.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {NavLink, Outlet, useParams } from "react-router-dom";
+import {Link, NavLink, Outlet, useParams } from "react-router-dom";
 
 export default function HostVansDetailLayout() {
   const id = useParams().id;
@@ -16,10 +16,11 @@ export default function HostVansDetailLayout() {
     rugged = "bg-[#115E59]";
   return (
     <div className="p-10">
+        <Link to='..' relative="path">&larr; <span className="underline text-lg">Back to all vans</span></Link>
       {van && (
-        <div className=" bg-white p-10">
-          <div className="gap-5 text-xl grid sm:grid-cols-[25%_60%] items-center">
-            <img src={van.imageUrl} alt="" className="rounded-md" />
+        <div className=" bg-white p-10 my-10">
+          <div className="gap-5 text-xl flex flex-wrap items-center">
+            <img src={van.imageUrl} alt="" className="rounded-md w-80" />
             <div className="flex flex-col gap-5">
               <button
                 className={` w-max text-xl text-center px-5 py-2 rounded-lg text-[#FFEAD0] ${
