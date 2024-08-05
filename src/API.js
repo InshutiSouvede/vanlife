301ddb00e351f54c4d9f18f669f2a4bd30831200
InshutiSouvede <isouvede2@gmail.com>
2024-08-05 09:28:15 +0200

    Add api for host vans

--- src/API.js
+++ src/API.js
@@ -10,6 +10,18 @@ export  async function getAllVans(){
     const data = await res.json()
     return data.vans
 }
+export  async function getHostVans(){
+    const res = await fetch('/api/host/vans')
+    if(!res.ok){
+        throw {
+            message:"Failed to fetch vans",
+            statusText:res.statusText,
+            status:res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
 export async function getVanById(id) {
     const  res = await fetch(`/api/vans/${id}`)
     const data = await res.json()
