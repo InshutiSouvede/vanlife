77e45f5dc074805bde773152bb87ab8715d55138
InshutiSouvede <isouvede2@gmail.com>
2024-08-06 16:00:40 +0200

    Defer , Await and Suspense host vans data

--- src/components/pages/hosts/vans/HostVans.jsx
+++ src/components/pages/hosts/vans/HostVans.jsx
@@ -1,36 +1,41 @@
-import { Link, redirect, useLoaderData, useLocation, useNavigate, useParams } from "react-router-dom";
+import { Await, defer, Link, useLoaderData,  } from "react-router-dom";
 import { getHostVans } from "../../../../API";
 import { checkLoginStatus } from "../../../../../utils";
+import { Suspense } from "react";
 
 export function hostVanLoader({request}) {  
   // checkLoginStatus(request)
-  return getHostVans();
+  return defer({hostVans:getHostVans()});
 }
 export default function HostVans({ children }) {
-  const loggedIn = false
-  const navigate = useNavigate()
-  const vans = useLoaderData();
-  console.log(vans);
-  const vansToDisplay = vans.map((el) => {
-    return (
-      <div key={el.id} className="flex items-center p-5 justify-between bg-white">
-        <Link  to={`/host/vans/${el.id}`} >
-          <div className="flex gap-5 items-center">
-            <img src={el.imageUrl} alt="van image" className="w-16" />
-            <div>
-              <p>{el.name}</p>
-              <p className="text-[#4D4D4D]">${el.price}/day</p>
+  const vansPromise = useLoaderData();
+  const renderHostVansElement = (vans)=>{
+    const vansToDisplay = vans.map((el) => {
+      return (
+        <div key={el.id} className="flex items-center p-5 justify-between bg-white">
+          <Link  to={`/host/vans/${el.id}`} >
+            <div className="flex gap-5 items-center">
+              <img src={el.imageUrl} alt="van image" className="w-16" />
+              <div>
+                <p>{el.name}</p>
+                <p className="text-[#4D4D4D]">${el.price}/day</p>
+              </div>
             </div>
-          </div>
-        </Link>
-        {children && children}
-      </div>
-    );
-  });
+          </Link>
+          {children && children}
+        </div>
+      );
+    });
+    return <div className="flex flex-col gap-5 my-5">{vansToDisplay}</div>
+  }
   return (
     <div className="px-10">
       <h1 className="font-bold text-3xl">Your listed vans</h1>
-      <div className="flex flex-col gap-5 my-5">{vansToDisplay}</div>
+      <Suspense fallback={<h3 className="font-bold text-3xl">Loading ...</h3>}>
+      <Await resolve={vansPromise.hostVans}>
+        {renderHostVansElement}
+      </Await>
+      </Suspense>
     </div>
   );
 }
