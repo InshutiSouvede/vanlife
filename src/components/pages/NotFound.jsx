b44de09c9281bc858e923315115d0d0f8fccf588
InshutiSouvede <isouvede2@gmail.com>
2024-08-06 09:17:13 +0200

    Improve not found page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound(){
     return (
-        <>
-        <h1 className="font-semibold text-center text-3xl">Page Not found</h1>
-        </>
+        <div className="flex flex-col gap-5 items-center px-20 my-20">
+        <h1 className="font-semibold text-left text-3xl">Sorry, the page you were looking for was not found.</h1>
+        <Link to='/' className="bg-black py-4 w-full  text-center text-xl rounded-lg text-white font-bold">Return to home</Link>
+        </div>
     )
 }
